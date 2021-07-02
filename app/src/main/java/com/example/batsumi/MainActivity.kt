4a2d720bca32aca84e0d5419a8c640ac382e6c4f
tumoresm <tumoresm@gmail.com>
2021-07-02 18:33:46 +0200

    initial, no separate activity for splash and intent is 1

---
+++ app/src/main/java/com/example/batsumi/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.batsumi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setTheme(R.style.Theme_Batsumi)
+        setContentView(R.layout.activity_main)
+    }
+}
